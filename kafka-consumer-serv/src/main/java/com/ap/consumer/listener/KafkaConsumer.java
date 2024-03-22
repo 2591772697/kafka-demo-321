969800326b3ac20fe68087278a24fb225c6d7f3e
xeno.cao@asiapay.com-at-452588258517 <xeno.cao@asiapay.com-at-452588258517>
2024-03-22 13:43:52 +0800

    producer finished

---
+++ kafka-consumer-serv/src/main/java/com/ap/consumer/listener/KafkaConsumer.java
@@ -0,0 +1,23 @@
+package com.ap.consumer.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * kafka consumer
+ **/
+@Component
+@Slf4j
+public class KafkaConsumer
+{
+ 
+    @KafkaListener(topics = "${kafka.topic.name}")
+    public void listen(ConsumerRecord<?, ?> record) {
+        log.info("consumption arrived :topic={}, offset={}, message={}", record.topic(), record.offset(), record.value());
+    }
+ 
+ 
+}
+ 

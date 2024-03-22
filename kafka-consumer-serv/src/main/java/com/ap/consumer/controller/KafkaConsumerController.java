9ed03c9480ed6fd69f41687768896dcf2c0bf2a2
xeno.cao@asiapay.com-at-452588258517 <xeno.cao@asiapay.com-at-452588258517>
2024-03-22 16:38:32 +0800

    one consumer instance get msgs

--- kafka-consumer-serv/src/main/java/com/ap/consumer/controller/KafkaConsumerController.java
+++ kafka-consumer-serv/src/main/java/com/ap/consumer/controller/KafkaConsumerController.java
@@ -6,24 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/consumer")
 public class KafkaConsumerController {
 
-//    private final KafkaConsumerService kafkaConsumerService;
-//
-//    @Autowired
-//    public KafkaConsumerController(KafkaConsumerService kafkaConsumerService) {
-//        this.kafkaConsumerService = kafkaConsumerService;
-//    }
     @Autowired
     KafkaConsumerService kafkaConsumerService;
 
     // 获取特定主题的最后10条消息  
-    @GetMapping("/messages/{topic}")
+    @GetMapping("/getLastMessages/{topic}")
     public ResponseEntity<List<Message>> getLastMessages(@PathVariable String topic) {
         List<Message> messages = kafkaConsumerService.getLastMessages(topic);
         return ResponseEntity.ok(messages);
--- kafka-consumer-serv/src/main/java/com/ap/consumer/service/KafkaConsumerService.java
+++ kafka-consumer-serv/src/main/java/com/ap/consumer/service/KafkaConsumerService.java
@@ -2,40 +2,44 @@ package com.ap.consumer.service;
 
 import com.ap.consumer.pojo.Message;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class KafkaConsumerService {
 
     private final ConcurrentHashMap<String, List<Message>> messages = new ConcurrentHashMap<>();
-    private final AtomicLong counter = new AtomicLong();
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
+
 
     // 监听Topic A的消息
     @KafkaListener(topics = "topic-A", groupId = "my-group")
     public void listenTopicA(ConsumerRecord<?, ?> record) {
+        String topic = record.topic();
+        String value = (String) record.value();
+        logger.info("Received message from topic {}: {}", topic, value);
         processMessage(record, "topic-A");
     }
 
     // 监听Topic B的消息
     @KafkaListener(topics = "topic-B", groupId = "my-group")
     public void listenTopicB(ConsumerRecord<?, ?> record) {
-        System.out.println("record.topic().toString() = " + record.topic().toString());
-        System.out.println("record.value().toString() = " + record.value().toString());
         processMessage(record, "topic-B");
     }
 
     private void processMessage(ConsumerRecord<?, ?> record, String topic) {
         String message = (String) record.value();
-        messages.computeIfAbsent(topic, k -> new LinkedList<>()).add(new Message(message, System.currentTimeMillis()));
+        List<Message> topicMessages = messages.computeIfAbsent(topic, k -> new LinkedList<>());
+        topicMessages.add(new Message(message, System.currentTimeMillis()));
         // 保持只显示最后10条消息
-        while (messages.get(topic).size() > 10) {
-            messages.get(topic).remove(0);
+        while (topicMessages.size() > 10) {
+            topicMessages.remove(0);
         }
     }
 
